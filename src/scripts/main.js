b92eb52b49d9576153bf0bb0b9f12897e3d83f0c
Шелков Дмитрий <bobyorrr@gmail.com>
2020-12-16 13:24:46 +0300

    + header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,15 @@
-let text = "Привет Васян";
-console.log(text);
+const activeClass = "active";
+
+// data-active
+const dataActiveControl = document.querySelectorAll("[data-active-control]");
+
+if (dataActiveControl) {
+	for (const activeControl of dataActiveControl) {
+		const activeBlock = document.querySelector(`[data-active-block="${activeControl.dataset.activeControl}"]`);
+
+		activeControl.addEventListener("click", () => {
+			activeControl.classList.toggle(activeClass);
+			activeBlock.classList.toggle(activeClass);
+		});
+	}
+}
